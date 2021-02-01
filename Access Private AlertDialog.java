2e06b8025f9711534c000ad42d64ee121617698d
hiishh <65687457+hiishh@users.noreply.github.com>
2021-02-01 12:00:39 +0000

    Update Access Private AlertDialog.java

--- Access Private AlertDialog.java
+++ Access Private AlertDialog.java
@@ -1,9 +1,9 @@
     private static AlertDialog createAlertDialog(Context context) {
         try {
-            //alternative method alertdialog (by dise?)
+            //alternative method alertdialog (by TheDise.me)
             Constructor declaredConstructor = AlertDialog.class.getDeclaredConstructor(Context.class);
             declaredConstructor.setAccessible(true);
-            //allowing access to alertdialog private class (as alertdialog.builder non accessible)
+            //allowing access to alertdialog private class (as alertdialog.builder non accessible, maybe due to ProGuard class renaming)
             return (AlertDialog) declaredConstructor.newInstance(context);
         } catch (Exception unused) {
             return null;
